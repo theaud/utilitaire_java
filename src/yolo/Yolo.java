64f65da9159e6fdf29401ae5747b66929fd41c4a
theauda <brionmathieu@gmail.com>
2016-01-27 19:37:07 +0100

    test

---
+++ src/yolo/Yolo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yolo;
+import java.util.*;
+/**
+ *
+ * @author mathieu
+ */
+public class Yolo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     Scanner scan = new Scanner(System.in);
+     
+     int u=util3.scanner.scanINT(scan);
+     System.out.println(u);
+        
+    }
+    
+}
