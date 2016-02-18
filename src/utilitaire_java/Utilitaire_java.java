5cf8d4c67c7595e2817f75bec5c2d0ac9a2206eb
theauda <brionmathieu@gmail.com>
2016-02-18 09:49:13 +0100

    premier modif

---
+++ src/utilitaire_java/Utilitaire_java.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilitaire_java;
+
+import graphique.Fenetre;
+import java.util.*;
+/**
+ *
+ * @author mathieu
+ */
+public class Utilitaire_java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Fenetre d=new Fenetre();
+        
+    }
+    
+}
